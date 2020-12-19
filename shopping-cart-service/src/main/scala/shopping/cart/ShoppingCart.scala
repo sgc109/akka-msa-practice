a15fab8adceffaedfa32fcbee8a47b58b369fbfc
Sungho Hwang <sgc109109@gmail.com>
2020-12-20 04:34:20 +0900

    akka MSA tutorial completed until chater 4

---
+++ shopping-cart-service/src/main/scala/shopping/cart/ShoppingCart.scala
@@ -0,0 +1,96 @@
+package shopping.cart
+
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, SupervisorStrategy}
+import akka.cluster.sharding.typed.scaladsl.{ClusterSharding, Entity, EntityTypeKey}
+import akka.pattern.StatusReply
+import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior, ReplyEffect, RetentionCriteria}
+
+import scala.concurrent.duration.DurationInt
+
+object ShoppingCart {
+
+  sealed trait Command extends CborSerializable
+
+  final case class AddItem(
+      itemId: String,
+      quantity: Int,
+      replyTo: ActorRef[StatusReply[Summary]]) extends Command
+
+  final case class Summary(items: Map[String, Int]) extends CborSerializable
+
+  sealed trait Event extends CborSerializable {
+    def cartId: String
+  }
+
+  final case class ItemAdded(cartId: String, itemId: String, quantity: Int) extends Event
+
+  final case class State(items: Map[String, Int]) extends CborSerializable {
+
+    def hasItem(itemId: String): Boolean = items.contains(itemId)
+
+    def isEmpty: Boolean = items.isEmpty
+
+    def updateItem(itemId: String, quantity: Int): State = {
+      quantity match {
+        case 0 => copy(items = items - itemId)
+        case _ => copy(items = items + (itemId -> quantity))
+      }
+    }
+  }
+
+  object State {
+    val empty: State = State(items = Map.empty)
+  }
+
+  private def handleCommand(
+      cartId: String,
+      state: State,
+      command: Command): ReplyEffect[Event, State] = {
+    command match {
+      case AddItem(itemId, quantity, replyTo) =>
+        if (state.hasItem(itemId))
+          Effect.reply(replyTo)(
+            StatusReply.Error(
+              s"Item '$itemId' was already added to this shopping cart"))
+        else if (quantity <= 0)
+          Effect.reply(replyTo)(
+            StatusReply.Error("Quantity must be greater than zero"))
+        else
+          Effect
+            .persist(ItemAdded(cartId, itemId, quantity))
+            .thenReply(replyTo) { updatedCart =>
+              StatusReply.Success(Summary(updatedCart.items))
+            }
+    }
+  }
+
+  private def handleEvent(state: State, event: Event) = {
+    event match {
+      case ItemAdded(_, itemId, quantity) =>
+        state.updateItem(itemId, quantity)
+    }
+  }
+
+  val entityKey: EntityTypeKey[Command] =
+    EntityTypeKey[Command]("ShoppingCart")
+
+  def init(system: ActorSystem[_]): Unit = {
+    ClusterSharding(system).init(Entity(entityKey) { entityContext =>
+      ShoppingCart(entityContext.entityId)
+    })
+  }
+
+  def apply(cartId: String): Behavior[Command] = {
+    EventSourcedBehavior
+      .withEnforcedReplies[Command, Event, State](
+        persistenceId = PersistenceId(entityKey.name, cartId),
+        emptyState = State.empty,
+        commandHandler = (state, command) => handleCommand(cartId, state, command),
+        eventHandler = (state, event) => handleEvent(state, event))
+      .withRetention(RetentionCriteria
+        .snapshotEvery(numberOfEvents = 100, keepNSnapshots = 3))
+      .onPersistFailure(
+        SupervisorStrategy.restartWithBackoff(200.millis, 5.seconds, 0.1))
+  }
+}
---
+++ shopping-cart-service/src/main/scala/shopping/cart/ShoppingCartServer.scala
@@ -0,0 +1,46 @@
+package shopping.cart
+
+import akka.actor.typed.ActorSystem
+import akka.grpc.scaladsl.{ServerReflection, ServiceHandler}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
+object ShoppingCartServer {
+
+  def start(
+      interface: String,
+      port: Int,
+      system: ActorSystem[_],
+      grpcService: proto.ShoppingCartService): Unit = {
+    implicit val sys: ActorSystem[_] = system
+    implicit val ec: ExecutionContext = system.executionContext
+
+    val service: HttpRequest => Future[HttpResponse] =
+      ServiceHandler.concatOrNotFound(
+        proto.ShoppingCartServiceHandler.partial(grpcService),
+        ServerReflection.partial(List(proto.ShoppingCartService))
+      )
+
+    val bound =
+      Http()
+      .newServerAt(interface, port)
+      .bind(service)
+      .map(_.addToCoordinatedShutdown(3.seconds))
+
+    bound.onComplete {
+      case Success(binding) =>
+        val address = binding.localAddress
+        system.log.info(
+          "Shopping online at gRPC server {}:{}",
+          address.getHostString,
+          address.getPort)
+      case Failure(ex) =>
+        system.log.error("Failed to bind gRPC endpoint, terminating system", ex)
+        system.terminate()
+    }
+  }
+}
